471642a2baa0a3815bfe8397502144442d3d7357
Borsódi Máté <borsodimate@turr.hu>
2025-02-26 13:00:31 +0100

    Első teszt commit

---
+++ iktfestekbolt/asd.lua
@@ -0,0 +1,10 @@
+/sorkeszites
+  |-- index.html
+  |-- rolunk.html
+  |-- faanyagok.html
+  |-- festekek.html
+  |-- bejelentkezes.html
+  |-- stilus.css
+  |-- images
+      |-- header-bg.jpg
+      |-- icon.jpg
